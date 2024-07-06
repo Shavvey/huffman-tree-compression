d653000f5f924231b255f9ad12f4beea7e39cb3a
Cole Johnson <coledjohn@gmail.com>
2024-07-06 00:22:10 -0600

    implementing printing

--- src/minheap.rs
+++ src/minheap.rs
@@ -1,12 +1,28 @@
 #![allow(dead_code)]
 
-struct MinHeap {
-    arr: Vec<u32>, // avoding using generics for now..
+#[derive(Copy, Clone, Debug)]
+pub struct Node {
+    item: char,
+    code: u32,
+    count: u32,
+}
+
+impl Node {
+    pub fn print(&self) {
+        println!("Character encoded: {}", self.item);
+        println!("Binary code: {:b}", self.code);
+        println!("Occurences: {}", self.count);
+    }
+}
+
+pub struct MinHeap {
+    arr: Vec<Node>, // avoding using generics for now..
     capacity: u32,
     size: u32,
 }
 
 impl MinHeap {
+    // create the minheap
     pub fn new(capacity: u32) -> MinHeap {
         MinHeap {
             arr: Vec::with_capacity(capacity as usize),
@@ -14,19 +30,36 @@ impl MinHeap {
             size: 0,
         }
     }
-
-    pub fn left(&self, index: u32) -> Option<u32> {
+    // gets the left child in the minheap
+    pub fn left_child(&self, index: u32) -> Option<&Node> {
         let l_item = 2 * index + 1;
+        // check to see if we have overindexed the array
         if l_item > self.size {
             return None;
         }
-        Some(self.arr[l_item as usize])
+        Some(&self.arr[l_item as usize])
     }
-    pub fn right(&self, index: u32) -> Option<u32> {
+
+    // gets the right child in the minheap
+    pub fn right_child(&self, index: u32) -> Option<&Node> {
         let r_item = 2 * index;
+        // check to see if we have overindex the array
         if r_item > self.size {
             return None;
         }
-        Some(self.arr[r_item as usize])
+        Some(&self.arr[r_item as usize])
+    }
+
+    pub fn get(&self, index: u32) -> Option<&Node> {
+        if index > self.size {
+            return None;
+        }
+        Some(&self.arr[index as usize])
+    }
+
+    pub fn print(&self) {
+        println!("{:?}", self.arr);
+        println!("Capacity: {}", self.capacity);
+        println!("Size: {}", self.size);
     }
 }
