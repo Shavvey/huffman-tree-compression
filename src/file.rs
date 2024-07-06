5b139f39ce035a9075c40d50c95a450114e00d29
Cole Johnson <coledjohn@gmail.com>
2024-07-06 00:52:52 -0600

    working on building heap

--- src/file.rs
+++ src/file.rs
@@ -1,3 +1,5 @@
+use crate::minheap::MinHeap;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
 pub fn get_lines(file_path: &str) -> Vec<String> {
@@ -8,4 +10,19 @@ pub fn get_lines(file_path: &str) -> Vec<String> {
     }
     // return the result
     result
+}
+
+pub fn get_heap(file_path: &str) -> MinHeap {
+    let mut min_heap = MinHeap::new(5);
+    let mut map: HashMap<char, u32> = HashMap::new();
+    let lines = get_lines(file_path);
+    for line in lines {
+        for char in line.chars() {
+            match &map.get(&char) {
+                Some(_) => todo!(),
+                None => todo!(),
+            }
+        }
+    }
+    min_heap
 }
