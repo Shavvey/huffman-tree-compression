606596334a74baa93747631dfb106864a3ddad66
Cole Johnson <coledjohn@gmail.com>
2024-07-05 22:39:14 -0600

    adding some files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{char, fs::File, io::Read};
+pub mod minheap;
+
 fn main() {
     println!("Hello, world!");
+}
+fn read_file(file_path: &str) {
+    // create file struct
+    let file: File = File::create(file_path).unwrap();
+    if let bytes = file.bytes() {
+        for byte in bytes {
+            let c: char = byte.into();
+        }
+    }
 }
