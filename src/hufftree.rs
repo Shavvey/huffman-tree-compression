4fa24174b99b232bf2ec639729a04bb9570362ea
Cole Johnson <coledjohn@gmail.com>
2024-07-12 02:20:05 -0600

    stuff

--- src/hufftree.rs
+++ src/hufftree.rs
@@ -19,10 +19,15 @@ pub struct HuffTree {
 }
 
 impl HuffTree {
+    // function to build the hufftree from
+    // bare min_heap, we do just by inserting
+    // intermediates nodes such that each node
+    // now represents a leaf on the tree
     pub fn build(min_heap: &MinHeap) -> HuffTree {
         todo!();
     }
 
+    // creates a very simple string representation of the hufftree
     pub fn to_string(&self) -> String {
         let mut result = String::new();
         self.tree.iter().for_each(|node| result.push(node.item));
